11153d6e71e4a589ec390da346407b87f8151876
IonizedBanana <cjbscouts29@gmail.com>
2025-12-15 11:37:30 -0500

    it functions, yay

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use serde::{Deserialize, Serialize};
+use std::fs::{self, File, read_to_string};
 use std::io;
+use std::io::prelude::*;
 
+// #[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Item {
     name: String,
     description: String,
     damage: String,
     healing: String,
+    value: String,
 }
 
 impl Item {
@@ -20,8 +26,11 @@ impl Item {
         if !self.healing.is_empty() {
             println!("item Healing: {}", self.healing);
         }
+        if !self.value.is_empty() {
+            println!("item value: {}", self.value);
+        }
     }
-    
+
     fn edit_name(&mut self) {
         self.name = String::from(get_input("enter a new name:").trim());
     }
@@ -37,8 +46,13 @@ impl Item {
     fn edit_healing(&mut self) {
         self.healing = String::from(get_input("enter a new healing amount:").trim());
     }
+
+    fn edit_value(&mut self) {
+        self.value = String::from(get_input("enter a new value:").trim());
+    }
 }
 
+#[derive(Serialize, Deserialize)]
 struct Note {
     title: String,
     body: String,
@@ -89,31 +103,39 @@ fn get_action(message: &str) -> i32 {
     }
 }
 
+fn print_notes(notebook: &Vec<Note>) {
+    let mut index = 1;
+    for note in &*notebook {
+        println!("{}. {}", index, note.title);
+        index += 1;
+    }
+}
+
 fn view_notes(notebook: &mut Vec<Note>) {
     loop {
         clear();
-        let mut index = 1;
-        for note in & *notebook {
-            println!("{}. {}", index, note.title);
-            index += 1;
-        }
+        print_notes(&notebook);
         let input = get_action(
-            "which note would you like to view? press enter to go back, or enter -1 to edit",
+            "which note would you like to view? press enter to go back, enter -1 to edit, or enter -2 to remove a note",
         );
         if input == 0 {
             break;
         } else if input == -1 {
             let which = get_action("which note would you like to edit?");
-            let title_or_body = get_action(
-                "would you like to edit the title or body? enter 1 for title, enter 2 for body",
-            );
+            let title_or_body =
+                get_action("which field would you like to edit?\n1. Title\n2. Body");
             let which = which as usize;
             if title_or_body == 1 {
                 notebook[which - 1].edit_title();
             } else if title_or_body == 2 {
                 notebook[which - 1].edit_body();
-            } else {
             }
+        } else if input == -2 {
+            clear();
+            print_notes(&notebook);
+            let which = get_action("which note do you want to remove?");
+            let which = which as usize;
+            notebook.remove(which - 1);
         } else {
             let input = input as usize;
             notebook[input - 1].print();
@@ -127,37 +149,43 @@ fn add_note(notebook: &mut Vec<Note>) {
     let title = title.trim();
     let body = get_input("please enter the body of the note:");
     let body = body.trim();
-    let mut note = Note {
+    let note = Note {
         title: String::from(title),
         body: String::from(body),
     };
     notebook.push(note);
 }
 
+fn print_items(inventory: &Vec<Item>) {
+    let mut index = 1;
+    for item in inventory {
+        println!("{}. {}", index, item.name);
+        if !item.damage.is_empty() && !item.healing.is_empty() {
+            println!("-> {} damage, {} healing", item.damage, item.healing);
+        } else if !item.healing.is_empty() {
+            println!("-> {} healing", item.healing);
+        } else if !item.damage.is_empty() {
+            println!("-> {} damage", item.damage);
+        }
+        index += 1;
+    }
+}
+
 fn list_items(inventory: &mut Vec<Item>) {
     loop {
         clear();
-        let mut index = 1;
-        for item in & *inventory {
-            println!("{}. {}", index, item.name);
-            if !item.damage.is_empty() && !item.healing.is_empty() {
-                println!("-> {} damage, {} healing", item.damage, item.healing);
-            } else if !item.healing.is_empty() {
-                println!("-> {} healing", item.healing);
-            } else if !item.damage.is_empty() {
-                println!("-> {} damage", item.damage);
-            }
-            index += 1;
-        }
+        print_items(&inventory);
         let input = get_action(
-            "which item would you like to view? press enter to go back, or enter -1 to edit",
+            "which item would you like to view? press enter to go back, enter -1 to edit, or enter -2 to remove an item",
         );
         if input == 0 {
             break;
         } else if input == -1 {
-            let which = get_action("which note would you like to edit?");
+            clear();
+            print_items(&inventory);
+            let which = get_action("which item would you like to edit?");
             let which_field = get_action(
-                "which field would you like to edit?\n1. Name\n2. Description\n3. Damage\n4. Healing",
+                "which field would you like to edit?\n1. Name\n2. Description\n3. Damage\n4. Healing\n5. value",
             );
             let which = which as usize;
             if which_field == 1 {
@@ -168,7 +196,15 @@ fn list_items(inventory: &mut Vec<Item>) {
                 inventory[which - 1].edit_damage();
             } else if which_field == 4 {
                 inventory[which - 1].edit_healing();
+            } else if which_field == 5 {
+                inventory[which - 1].edit_value();
             }
+        } else if input == -2 {
+            clear();
+            print_items(&inventory);
+            let which = get_action("which item do you want to remove?");
+            let which = which as usize;
+            inventory.remove(which - 1);
         } else {
             let input = input as usize;
             inventory[input - 1].print();
@@ -187,34 +223,143 @@ fn add_item(inventory: &mut Vec<Item>) {
     let damage = damage.trim();
     let healing = get_input("enter the healing, if applicable");
     let healing = healing.trim();
-    let mut item = Item {
+    let value = get_input("enter the value, if known");
+    let value = value.trim();
+    let item = Item {
         name: String::from(name),
         description: String::from(description),
         damage: String::from(damage),
         healing: String::from(healing),
+        value: String::from(value),
     };
     inventory.push(item);
 }
 
+fn make_save() -> File {
+    let save_file = File::create("DnD_save.json").expect("Could not make file!");
+    save_file
+}
+
+fn open_save(path: &str) -> File {
+    let save_exists: bool = match fs::exists("DnD_save_old.json") {
+        Ok(true) => true,
+        Ok(false) => false,
+        Err(_) => false,
+    };
+    if save_exists {
+        fs::remove_file("DnD_save_old.json").expect("cant remove ts ah file");
+    }
+    let old_contents = fs::read_to_string(path).expect("fuck you bitch");
+    let mut old_file = File::create("DnD_save_old.json").expect("221 cant create file");
+    old_file
+        .write(old_contents.as_bytes())
+        .expect("222 cabt write ts ah");
+    fs::remove_file(path).expect("could not remove file");
+    let file = make_save();
+    file
+}
+
+fn save_inventory(inventory: &mut Vec<Item>, save_file: &mut File) {
+    save_file.write(b"*exists*\n").expect("240 couldnt write");
+    for item in inventory {
+        let serialized = serde_json::to_string(item).expect("221 couldnt write");
+        save_file
+            .write(serialized.as_bytes())
+            .expect("222 couldnt write");
+        save_file.write(b"\n").expect("223 couldnt write");
+    }
+    save_file.write(b"*end*\n").expect("225 couldnt write");
+}
+
+fn save_notebook(notebook: &mut Vec<Note>, save_file: &mut File) {
+    for note in notebook {
+        let serialized = serde_json::to_string(note).expect("230 couldnt write");
+        save_file
+            .write(serialized.as_bytes())
+            .expect("231 couldnt write");
+        save_file.write(b"\n").expect("232 couldnt write");
+    }
+}
+
+fn create_data(inventory: &mut Vec<Item>, notebook: &mut Vec<Note>, file_path: &str) {
+    let mut exists = false;
+    let mut end = false;
+    for line in read_to_string(file_path)
+        .expect("could not read file when creating inventory")
+        .lines()
+    {
+        if line == "*exists*" {
+            exists = true;
+            continue;
+        }
+        if !exists {
+            panic!("save corrupted! move \"DnD_save_old.json\" to \"DnD_save.json\"")
+        }
+
+        if line == "*end*" {
+            end = true;
+            continue;
+        }
+        if !end {
+            let deserialized = serde_json::from_str(line).expect("could not deserialize");
+            inventory.push(deserialized);
+        } else {
+            let deserialized = serde_json::from_str(line).expect("could not deserialize");
+            notebook.push(deserialized);
+        }
+    }
+}
+
+fn verify_data(file_path: &str) -> bool {
+    for line in read_to_string(file_path)
+        .expect("could not read file when creating inventory")
+        .lines()
+    {
+        if line == "*exists*" {
+            return true;
+        }
+    }
+    false
+}
+
 fn main() {
-    const OPTIONS: &str =
-        "please select an option:\n1. view inventory\n2. view notebook\n3. add item\n4. add note";
+    const SAVE_FILE_PATH: &str = "DnD_save.json";
+    const OPTIONS: &str = "please select an option:\n1. view inventory\n2. view notebook\n3. add item\n4. add note\n5. save and quit";
     let mut inventory: Vec<Item> = Vec::new();
     let mut notebook: Vec<Note> = Vec::new();
-
+    let mut save_file: File;
+    let save_exists: bool = match fs::exists(SAVE_FILE_PATH) {
+        Ok(true) => true,
+        Ok(false) => false,
+        Err(_) => false,
+    };
+    if save_exists {
+        let data_safe = verify_data(SAVE_FILE_PATH);
+        if !data_safe {
+            panic!("save corrupted! move \"DnD_save_old.json\" to \"DnD_save.json\"");
+        }
+        create_data(&mut inventory, &mut notebook, SAVE_FILE_PATH);
+        save_file = open_save(SAVE_FILE_PATH);
+    } else {
+        save_file = make_save();
+    }
     loop {
         clear();
         let action = get_action(OPTIONS);
         if action == 1 {
-            // item1.print();
             list_items(&mut inventory);
         } else if action == 2 {
-            // note1.print();
             view_notes(&mut notebook);
         } else if action == 3 {
             add_item(&mut inventory);
         } else if action == 4 {
             add_note(&mut notebook);
+        } else if action == 5 {
+            save_inventory(&mut inventory, &mut save_file);
+            save_notebook(&mut notebook, &mut save_file);
+            println!("writing data..");
+            wait();
+            break;
         } else {
             continue;
         }
